5f4831a0951162a21a2a88de4596121bedf44962
Alexandra Campo <alexandracampo86@gmail.com>
2023-02-24 21:46:59 +0100

    add functions DOM

--- src/js/main.js
+++ src/js/main.js
@@ -18,44 +18,51 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         renderList(dataList);
     })
 
-//funcion recorre el array datalList y los pinta con la función "renderEveryDrink". Pinta el listado de la búsqueda que hayamos hecho:
+//funcion recorre el array dataList y pinta los cócteles con la función "buildDrink":
 function renderList(dataList) {
     listResults.innerHTML = '';
     for (const drink of dataList) {
-        const drinkElement = buildDrink(drink);
-        listResults.appendChild(drinkElement);
+        const drinkElement = buildDrinkLi(drink);
+        listResults.appendChild(drinkElement); // esto de hacerle un appendchild a listResults sigo sin verlo
     }
 }
 
-// Función que pinta el código de cada cóctel (cada <li>):
-function buildDrink(drink) {
-    /*  let html = `<li class="card-drink select-item" id=${drink.idDrink}"> 
-     <h3 class="js-item-name"> ${drink.strDrink} </h3>
-     <img class="js-img-drink img-drink" src=${drink.strDrinkThumb} alt="Imagen cóctel">
-     </li>`;
-     return html; */
-    const elementLi = document.createElement('li');
-    elementLi.setAttribute('class', 'card-drink');
-    elementLi.setAttribute('class', 'select-item');
-    elementLi.setAttribute('id', drink.idDrink);
-
-
+// Funcion para construir el <h3> que va dentro de <li>:
+function buildDrinkH3(drink) {
     const elementH3 = document.createElement('h3');
     elementH3.setAttribute('class', 'js-item-name');
     const textH3 = document.createTextNode(drink.strDrink);
     elementH3.appendChild(textH3);
-    elementLi.appendChild(elementH3);
 
+    return elementH3;
+}
 
+// Funcion para construir la <img> que va dentro de <li>:
+function buildDrinkImg(drink) {
     const elementImg = document.createElement('img');
-    elementImg.setAttribute('class', 'js-img-drink');
-    elementImg.setAttribute('class', 'img-drink');
+    elementImg.setAttribute('class', "js-img-drink img-drink");
     elementImg.setAttribute('src', drink.strDrinkThumb);
     elementImg.setAttribute('alt', 'Imagen cóctel');
+
+    return elementImg;
+}
+
+// Función que construye el código HTML de <li>:
+function buildDrinkLi(drink) {
+    const elementLi = document.createElement('li');
+    elementLi.setAttribute('class', 'card-drink select-item');
+    elementLi.setAttribute('id', drink.idDrink);
+
+    //meto en variables ambas llamadas a las funciones
+    const elementH3 = buildDrinkH3(drink);
+    const elementImg = buildDrinkImg(drink);
+
+    // Indico que ambas son child de <li>
+    elementLi.appendChild(elementH3);
     elementLi.appendChild(elementImg);
 
-    // Añado un listener sobre li:
-    elementLi.addEventListener('click', handleClickFav)
+    // Añado un listener sobre <li>:
+    elementLi.addEventListener('click', handleClickFav);
 
     return elementLi;
 }
@@ -89,4 +96,22 @@ function handleClickFav(ev) {
     console.log(ev.currentTarget);
 }
 
+
+
 btnSearch.addEventListener('click', handleClick);
+
+
+
+
+
+
+
+
+
+
+
+/*  let html = `<li class="card-drink select-item" id=${drink.idDrink}">
+    <h3 class="js-item-name"> ${drink.strDrink} </h3>
+    <img class="js-img-drink img-drink" src=${drink.strDrinkThumb} alt="Imagen cóctel">
+    </li>`;
+    return html; */
